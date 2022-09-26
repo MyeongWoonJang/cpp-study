f564d6e8ad4b3f7f7683e8ac100d4cc9000e146e
Lcat <ljj7403@naver.com>
2022-09-26 20:51:26 +0900

    ?
    ?

---
+++ MyDx12/MyDx12/framework.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <windows.h>
+
+// dx12 헤더 파일
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+
+#include <string>
+#include <wrl.h>
+#include <shellapi.h>
+
+// C 런타임 헤더 파일입니다.
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
