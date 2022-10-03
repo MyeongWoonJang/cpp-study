a3221a6c50df5fff401d4f62f449f2416a1212f7
Lcat <ljj7403@naver.com>
2022-10-03 13:49:48 +0900

    Update main.cpp

--- dataStructure/Project1/Project1/main.cpp
+++ dataStructure/Project1/Project1/main.cpp
@@ -1,22 +1,67 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
 
-template <typename T>
-void print(const T& t)
-{
-	std::cout << t << std::endl;
-}
+using namespace std;
 
-template <typename First, typename... Rest>
-void print(const First& first, const Rest&... rest)
+struct Champion
 {
-	std::cout << first << ", ";
-	print(rest...);
-}
+	string		name;
+	int			hp;
+	int			mp;
+	int			attack;
+	int			defense;
+};
+
+random_device rd;
+mt19937 gen(rd());
+
+int random(int min, int max);
+void createData(int amount);
 
 int main()
 {
-	print(1);
-	print(10, 20);
-	print(100, 200, 300);
-	print("first", 2, "third", 3.14159);
+	createData(1000);
+}
+
+int random(int min, int max)
+{
+	uniform_int_distribution<> uid(min, max);
+
+	return uid(gen);
+}
+
+void createData(int amount)
+{
+	ofstream out;
+	out.open("data.txt");
+
+	string alphabet{ "abcdefghijklmnopqrstuvwxyz" };
+
+	for (int i = 0; i < amount; ++i)
+	{
+		// ·£´ýÇÑ ±æÀÌÀÇ ÀÌ¸§
+		string randomName;
+		int nameLength = random(5, 10);
+
+		for (int i = 0; i < nameLength; ++i)
+		{
+			int randomValue = random(0, 25);
+			randomName.push_back(alphabet[randomValue]);
+		}
+		randomName[0] = toupper(randomName[0]);
+
+		// ·£´ý hp
+		int randomHp = random(100, 200);
+		// ·£´ý mp
+		int randomMp = random(60, 120);
+		// ·£´ý attack
+		int randomAttack = random(50, 100);
+		// ·£´ý defense
+		int randomDefense = random(0, 20);
+
+		out << randomName << ' ' << randomHp << ' ' << randomMp
+			<< ' ' << randomAttack << ' ' << randomDefense << '\n';
+	}
 }
