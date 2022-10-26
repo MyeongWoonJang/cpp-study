0254d23e47c78e0a48a4f96355112b3c6412f447
Lcat <ljj7403@naver.com>
2022-10-26 21:04:19 +0900

    CS-222

--- assignment-sprint1-write-cpp-classes/assignment/Project1/my_string.cpp
+++ assignment-sprint1-write-cpp-classes/assignment/Project1/my_string.cpp
@@ -1,1 +1,3 @@
 #include "my_string.h"
+
+#include <string>
--- assignment-sprint1-write-cpp-classes/assignment/Project1/my_string.h
+++ assignment-sprint1-write-cpp-classes/assignment/Project1/my_string.h
@@ -6,9 +6,29 @@
 class my_string
 {
 public:
+	const char& at(const size_t n) const;
+	const char& front() const;
+	const char* c_str() const;
+	const char* data() const;
+	bool empty() const;
+	int size() const;
+	int length() const;
+	void reserve(const size_t n);
+	size_t capacity();
+	void shrink_to_fit();
+	void resize(const size_t n);
+	void clear();
+	void insert(const size_t n, const std::string other);
+	void erase(const size_t n, const size_t m);
+	void push_back(const char c);
+	void pop_back();
+	void append(const char c);
+	int compare(const std::string& other) const;
+	void replace(const std::string& A, const std::string B);
+	const my_string& substr(const size_t n, const size_t m);
+	int find(const std::string str) const;
 
 private:
 
 };
-
 #endif
