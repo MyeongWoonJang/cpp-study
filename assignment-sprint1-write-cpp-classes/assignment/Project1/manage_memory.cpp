be861a33a0c3bc7a465a25d820e53e33ed07969b
Lcat <ljj7403@naver.com>
2022-10-31 20:42:34 +0900

    CS-1 CS-374 CS-383
    함수 콜스택 추적 완성

--- assignment-sprint1-write-cpp-classes/assignment/Project1/manage_memory.cpp
+++ assignment-sprint1-write-cpp-classes/assignment/Project1/manage_memory.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
 
+int subfunc1(short);    // function declaration
+char subfunc2(int, char);   // function declaration
+
 int main()
 {
     std::cout << __FUNCTION__ << " entry\n";    
 
     int ret = 0;
+    int func1{};
+    char func2{};
+
+    func1 = subfunc1(100);
+    func2 = subfunc2(4, 'c');
 
     std::cout << __FUNCTION__ << " exit\n";
 
     return ret;        
+}
+
+int subfunc1(short d)   // function definition
+{
+    std::cout << __FUNCTION__ << " entry\n";    // output: func entry
+
+    float e = 2.7f;
+    bool f = true;
+
+    std::cout << __FUNCTION__ << " exit\n";     // output: func exit
+
+    return 0;
+}
+
+char subfunc2(int g, char h)    // function definition
+{
+    std::cout << __FUNCTION__ << " entry\n";    // output: func entry
+
+    double i = 1.017;
+
+    std::cout << __FUNCTION__ << " exit\n";     // output: func exit
+
+    return 'f';
 }
