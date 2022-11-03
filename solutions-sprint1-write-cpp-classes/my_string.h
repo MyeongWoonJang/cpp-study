ace093fd1dcfcbb887a41a11ec3e681ef981e37d
Woon2 <73771162+Woon-2@users.noreply.github.com>
2022-11-03 22:43:01 +0900

    declared constructors of my_string
    CS-1 CS-113 CS-143

--- solutions-sprint1-write-cpp-classes/my_string.h
+++ solutions-sprint1-write-cpp-classes/my_string.h
@@ -7,7 +7,16 @@ template <class CharT>
 class my_string
 {
 public:
-
+    my_string() noexcept;
+    my_string(std::size_t, CharT);
+    my_string(const my_string&, std::size_t);
+    my_string(const my_string&, std::size_t, std::size_t);
+    my_string(const CharT*, std::size_t);
+    my_string(const CharT*);
+    my_string(const my_string&);
+    my_string(my_string&&);
+    my_string(std::nullptr_t) = delete;
+    
 private:
 }
 
