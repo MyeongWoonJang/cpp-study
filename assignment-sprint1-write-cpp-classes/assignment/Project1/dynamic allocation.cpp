fbd9a082ab001b1e67b268a23ac11be57234ad3a
Lcat <ljj7403@naver.com>
2022-11-05 00:26:47 +0900

    CS-1 CS-122 CS-386
    TODO - malloc/free와 new/delete의 차이 작성
    
    오류가 발생한 코드는 discussions에 작성 예정

--- assignment-sprint1-write-cpp-classes/assignment/Project1/dynamic allocation.cpp
+++ assignment-sprint1-write-cpp-classes/assignment/Project1/dynamic allocation.cpp
@@ -5,12 +5,61 @@ void printmem_stream(const Ty& var, std::ostream& os);  // function template dec
 template <typename Ty>
 void printmem(const Ty& var);   // function template declaration
 
-void memory_leak();
-void dangling_pointer();
+//void memory_leak();
+//void dangling_pointer();
+
+void* operator new(std::size_t sz);
+void operator delete(void* ptr) noexcept;
 
 int main()
 {
-    dangling_pointer();
+	// dynamic allocation
+	// type* name = new type{initialization value};
+	int* heap_int = new int{ 123 };
+
+	printmem(heap_int);
+	printmem(*heap_int);
+
+	// dynamic deallocation
+	// delete name;
+	delete heap_int;
+
+	// array dynamic allocation
+	// type* name = new int[number of elements]{initialization values};
+	int* heap_arr = new int[5] {1, 2, 3, 4, 5};
+
+	printmem(heap_arr);
+	printmem(*heap_arr);
+
+	for (int i = 0; i < 5; ++i)
+		printmem(heap_arr[i]);
+
+	// array dynamic deallocation
+	// delete[] name;
+	delete[] heap_arr;
+}
+
+void* operator new(std::size_t sz)
+{
+	std::cout << __FUNCTION__ << " entry\n";
+
+	if (sz == 0)
+		++sz; // avoid std::malloc(0) which may return nullptr on success
+
+	if (void* ptr = std::malloc(sz))  // C++17, if statement with initializer
+	{
+		std::cout << __FUNCTION__ << " exit\n";
+		return ptr;
+	}
+
+	throw std::bad_alloc{}; // required by [new.delete.single]/3
+}
+
+void operator delete(void* ptr) noexcept
+{
+	std::cout << __FUNCTION__ << " entry\n";
+	std::free(ptr);
+	std::cout << __FUNCTION__ << " exit\n";
 }
 
 // 변수를 전달하면 그 변수의 메모리 개요를 출력합니다.
@@ -30,18 +79,18 @@ void printmem(const Ty& var)    // function template definition
     printmem_stream(var, std::cout);
 }
 
-void memory_leak()
-{
-    char* mem = new char[1'000'000'000] {0};
-
-    // delete[] mem이 없어 1GB의 메모리가 누수됩니다.
-}
-
-void dangling_pointer()
-{
-    char* mem = new char[1000] {0};  // 여기서 할당됩니다.
-
-    delete[] mem;   // 여기서 반납됩니다.
-
-    std::cout << mem[100];  // 반납된 메모리에 접근해 미정의 동작이 발생합니다.
-}
+//void memory_leak()
+//{
+//    char* mem = new char[1'000'000'000] {0};
+//
+//    // delete[] mem이 없어 1GB의 메모리가 누수됩니다.
+//}
+//
+//void dangling_pointer()
+//{
+//    char* mem = new char[1000] {0};  // 여기서 할당됩니다.
+//
+//    delete[] mem;   // 여기서 반납됩니다.
+//
+//    std::cout << mem[100];  // 반납된 메모리에 접근해 미정의 동작이 발생합니다.
+//}
