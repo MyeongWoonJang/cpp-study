16446fb946360fc8444930e724b05fef5c5ef3a5
Woon2 <73771162+Woon-2@users.noreply.github.com>
2022-11-06 18:25:32 +0900

    for chaining, now _check_out_of_range returns.
    CS-1 CS-113 CS-143

--- solutions-sprint1-write-cpp-classes/my_exception.h
+++ solutions-sprint1-write-cpp-classes/my_exception.h
@@ -14,15 +14,29 @@ namespace
     #define check_out_of_range(val, lo, hi, expr) _check_out_of_range((val), (lo), (hi), (expr), __FILE__, __LINE__, __FUNCTION__)
     #endif
  
-    void _throw_out_of_range(const char* expr, const char* file, int line, const char* func)
+    inline void _throw_out_of_range(const char* expr, const char* file, int line, const char* func)
     {
         throw std::out_of_range{_FMT("in %s:%d:%s:\n%s\n", file, line, func, expr)};
     }
- 
+    
     template <class T>
-    void _check_out_of_range(const T& val, const T& lo, const T& hi, const char* expr, const char* file, int line, const char* func)
+    inline void _no_chain_check_out_of_range(const T& val, const T& lo, const T& hi, const char* expr, const char* file, int line, const char* func)
     {
         if (std::clamp(val, lo, hi) != val) _throw_out_of_range(expr, file, line, func);
+    }
+ 
+    template <class T>
+    T& _check_out_of_range(T& val, const T& lo, const T& hi, const char* expr, const char* file, int line, const char* func)
+    {
+        _no_chain_check_out_of_range(val, lo, hi, expr, file, line, func);
+        return val;
+    }
+ 
+    template <class T>
+    const T& _check_out_of_range(const T& val, const T& lo, const T& hi, const char* expr, const char* file, int line, const char* func)
+    {
+        _no_chain_check_out_of_range(val, lo, hi, expr, file, line, func);
+        return val;
     }
  
     #endif   
