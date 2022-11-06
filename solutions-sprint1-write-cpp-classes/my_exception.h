017068bc4b5339b92ee03c5b15bac161f55d232e
Woon2 <73771162+Woon-2@users.noreply.github.com>
2022-11-06 18:20:59 +0900

    made check_out_of_range macro
    CS-1 CS-113 CS-143

--- solutions-sprint1-write-cpp-classes/my_exception.h
+++ solutions-sprint1-write-cpp-classes/my_exception.h
@@ -1,4 +1,31 @@
 #ifndef _my_exception
 #define _my_exception
 
+#include "my_format.h"
+#include <algorithm>
+
+namespace
+{
+    #ifdef _ACTIVATE_EXCEPTION_MACRO
+    
+    #ifdef __PRETTY_FUNCTION__
+    #define check_out_of_range(val, lo, hi, expr) _check_out_of_range((val), (lo), (hi), (expr), __FILE__, __LINE__, __PRETTY_FUNCTION__)
+    #else
+    #define check_out_of_range(val, lo, hi, expr) _check_out_of_range((val), (lo), (hi), (expr), __FILE__, __LINE__, __FUNCTION__)
+    #endif
+ 
+    void _throw_out_of_range(const char* expr, const char* file, int line, const char* func)
+    {
+        throw std::out_of_range{_FMT("in %s:%d:%s:\n%s\n", file, line, func, expr)};
+    }
+ 
+    template <class T>
+    void _check_out_of_range(const T& val, const T& lo, const T& hi, const char* expr, const char* file, int line, const char* func)
+    {
+        if (std::clamp(val, lo, hi) != val) _throw_out_of_range(expr, file, line, func);
+    }
+ 
+    #endif   
+}
+
 #endif
