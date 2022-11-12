b516075c777d5e216590a7b6041222d7419740c5
Woon2 <73771162+Woon-2@users.noreply.github.com>
2022-11-12 17:39:36 +0900

    replaced macro
    it is required to undefine exhausted macros.
    so when any other source needs the macros once more, redefinition of macro is required.
    redefinition never occurs in old version.
    
    CS-1 CS-113 CS-143

--- solutions-sprint1-write-cpp-classes/my_format.h
+++ solutions-sprint1-write-cpp-classes/my_format.h
@@ -1,22 +1,26 @@
+#ifndef __FUNCTION_NAME__
+    
+#ifdef __GNUC__     // if compiled by gcc/g++ use __PRETTY_FUNCTION__ for function name
+#define __FUNCTION_NAME__ __PRETTY_FUNCTION__
+#elif _MSC_VER      // elif compiled by msvc use __FUNCSIG__ for function name
+#define __FUNCTION_NAME__ __FUNCSIG__
+#else   // else use __FUNCTION__(standard) for function name
+#define __FUNCTION_NAME__ __FUNCTION__
+#endif
+    
+#endif
+
+// _FMT changes a formatted expression into a string(char*)
+#ifndef _FMT
+#define _FMT(...) _get_fmtbuf(sprintf(_get_fmtbuf(), ## __VA_ARGS__))
+#endif
+
 #ifndef _my_format
 #define _my_format
 
 #include <cstdio>
-
 namespace
 {
-    #ifndef __FUNCTION_NAME__
-    
-    #ifdef __GNUC__     // if compiled by gcc/g++ use __PRETTY_FUNCTION__ for function name
-    #define __FUNCTION_NAME__ __PRETTY_FUNCTION__
-    #elif _MSC_VER      // elif compiled by msvc use __FUNCSIG__ for function name
-    #define __FUNCTION_NAME__ __FUNCSIG__
-    #else   // else use __FUNCTION__(standard) for function name
-    #define __FUNCTION_NAME__ __FUNCTION__
-    #endif
-    
-    #endif
-    
     char* _get_fmtbuf(...)
     {
         static constexpr std::size_t FMTBUF_SIZE = 1 << 8;
@@ -24,9 +28,6 @@ namespace
         static char fmtbuf[FMTBUF_SIZE];
         return fmtbuf;
     }
-    
-    // _FMT changes a formatted expression into a string(char*)
-    #define _FMT(...) _get_fmtbuf(sprintf(_get_fmtbuf(), ## __VA_ARGS__))
 }
 
 #endif
