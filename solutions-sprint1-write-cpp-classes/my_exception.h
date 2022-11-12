b80369bbc16734d411b03da2c2c7d94544b205d6
Woon2 <73771162+Woon-2@users.noreply.github.com>
2022-11-12 20:47:21 +0900

    lambda captures perfect forwarded arguments
    CS-1 CS-113 CS-143

--- solutions-sprint1-write-cpp-classes/my_exception.h
+++ solutions-sprint1-write-cpp-classes/my_exception.h
@@ -13,10 +13,14 @@
 #ifndef debug_check_out_of_range
 
 #include "my_format.h"
+#include <algorithm>
+#include <type_traits>
+
+    #define _fw(val) std::forward<std::remove_reference_t<decltype(val)>>(val)
 
     #ifdef _DEBUG
         #define debug_check_out_of_range(val, lo, hi, expr) \
-        ([_val = val, _lo = lo, _hi = hi, _expr = expr, _file = __FILE__, _line = __LINE__, _func = __FUNCTION_NAME__]() \
+        ([_val = _fw(val), _lo = _fw(lo), _hi = _fw(hi), _expr = _fw(expr), _file = __FILE__, _line = __LINE__, _func = __FUNCTION_NAME__]() \
         { \
             if (std::clamp((_val), (_lo), (_hi)) != (_val)) \
                 throw std::out_of_range{_FMT("in %s:%d:%s:\n%s\n", _file, _line, _func, _expr)}; \
