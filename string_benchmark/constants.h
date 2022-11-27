f7a1ac5041fc97f6ce029a155bb45d794481de7f
Woon2 <wkdauddns9@gmail.com>
2022-11-27 16:39:26 +0900

    Update constants.h

--- string_benchmark/constants.h
+++ string_benchmark/constants.h
@@ -6,7 +6,7 @@ inline constexpr std::size_t RowCnt = 1'000;
 inline constexpr int MinAlpha = 'a';
 inline constexpr int MaxAlpha = 'z';
 inline constexpr int MinLen = 50;
-inline constexpr int MinLen = 200;
+inline constexpr int MaxLen = 200;
 inline constexpr char Delim = ' ';
 
 #endif // __constants
