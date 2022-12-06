b54f2efef7f8a3d0c1a784b1bc7d546818653292
Lcat <ljj7403@naver.com>
2022-12-06 20:38:32 +0900

    CS-1 CS-391 CS-396
    레퍼런스 반환 과제

--- assignment-sprint1-write-cpp-classes/assignment/Project1/go_for_reference.cpp
+++ assignment-sprint1-write-cpp-classes/assignment/Project1/go_for_reference.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+struct TestScore
+{
+    int math;
+    int english;
+    int physics;
+    int chemicals;
+};
+
 void swap(int& lhs, int& rhs)
 {
     // lhs: left hand side, rhs: right hand side
@@ -16,12 +24,86 @@ void swap(int& lhs, int& rhs)
     rhs = tmp;
 }
 
+TestScore& min(TestScore& lhs, TestScore& rhs)
+{
+    int lhs_sum = lhs.math + lhs.english + lhs.physics + lhs.chemicals;
+    int rhs_sum = rhs.math + rhs.english + rhs.physics + rhs.chemicals;
+
+    if (lhs_sum < rhs_sum)
+        return lhs;
+    else
+        return rhs;
+}
+
+TestScore& max(TestScore& lhs, TestScore& rhs)
+{
+    int lhs_sum = lhs.math + lhs.english + lhs.physics + lhs.chemicals;
+    int rhs_sum = rhs.math + rhs.english + rhs.physics + rhs.chemicals;
+
+    if (lhs_sum > rhs_sum)
+        return lhs;
+    else
+        return rhs;
+}
+
+TestScore& median_of_three(TestScore& one, TestScore& two, TestScore& three)
+{
+    int one_sum = one.math + one.english + one.physics + one.chemicals;
+    int two_sum = two.math + two.english + two.physics + two.chemicals;
+    int three_sum = three.math + three.english + three.physics + three.chemicals;
+
+    if (one_sum > two_sum)
+    {
+        if (one_sum > three_sum)
+        {
+            if (two_sum > three_sum)
+                return two;
+            else
+                return three;
+        }
+        else
+            return one;
+    }
+    else // one_sum < two_sum (같은 경우는 어찌 해야되지..?)
+    {
+        if (one_sum > three_sum)
+        {
+            return one;
+        }
+        else
+        {
+            if (two_sum > three_sum)
+                return three;
+            else
+                return two;
+        }
+    }
+}
+
 int main()
 {
-    int a = 3;
-    int b = 5;
+    TestScore a{ 20, 50, 30, 40 };
+    TestScore b{ 40, 60, 90, 70 };
+    TestScore c{ 10, 30, 60, 60 };
 
-    swap(a, b);
+    if (&a != &min(a, b) ||
+        &c != &min(b, c) ||
+        &a != &min(a, c))
+    {
+        std::cout << "min의 구현이 잘못되었습니다.\n";
+    }
 
-    std::cout << "a = " << a << ", b = " << b << '\n'; // output: a = 5, b = 3
+    if (&b != &max(a, b) ||
+        &b != &max(b, c) ||
+        &c != &max(a, c))
+    {
+        std::cout << "max의 구현이 잘못되었습니다.\n";
+    }
+
+    if (&c != &median_of_three(a, b, c))
+    {
+        std::cout << "median_of_three의 구현이 잘못되었습니다.\n";
+    }
+
+    std::cout << "\"...의 구현이 잘못되었습니다.\" 출력이 안 나왔다면 성공!\n";
 }
