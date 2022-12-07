f57ced8072bbe81a59ac587437c7874e670ed09b
orange2458 <asm1133@naver.com>
2022-12-08 00:43:24 +0900

    CS-1 CS-392

--- New Project/New Project/go_for_reference.cpp
+++ New Project/New Project/go_for_reference.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
-void swap( int& lhs, int& rhs );
+void swap( int* lhs, int* rhs );
 
 int main( )
 {
 	int a{ 3 };
 	int b{ 5 };
 
-	swap( a, b );
+	swap( &a, &b );
 
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 }
 
-void swap( int& lhs, int& rhs )
+void swap( int* lhs, int* rhs )
 {
-	int temp{ lhs };
-	lhs = rhs;
-	rhs = temp;
+	int temp{ *lhs };
+	*lhs = *rhs;
+	*rhs = temp;
 }
