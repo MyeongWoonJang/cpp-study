5ea5a5f5e60f70a7200ff9717dbe5b16251ed2f2
orange2458 <asm1133@naver.com>
2022-12-10 13:30:15 +0900

    CS-1 CS-378

--- New Project/New Project/manage_memory.cpp
+++ New Project/New Project/manage_memory.cpp
@@ -2,6 +2,8 @@
 
 int subfunc1( int );
 char subfunc2( float, char );
+void subfunc3(char j, int k, double l); // function declaration
+void subfunc4( );
 
 template <typename Ty>
 void printmem_stream( const Ty& var, std::ostream& os );  // function template declaration
@@ -37,6 +39,8 @@ int main( )
 
 	var1 = subfunc1( 1 );
 	var2 = subfunc2( 1.1f, 'n' );
+	subfunc3( 'j', 3, 12.3 );
+	subfunc4( );
 
 	std::cout << __FUNCTION__ << " exit\n";
 }
@@ -72,4 +76,35 @@ char subfunc2( float d, char e )
 	std::cout << __FUNCTION__ << " exit\n";
 
 	return 'q';
+}
+
+void subfunc3( char j, int k, double l ) // function definition
+{
+	std::cout << __FUNCTION__ << " entry\n";
+
+	char* pj = &j;
+	int* pk = &k;
+	double* pl = &l;
+
+	std::cout << "pj: [" << static_cast<void*>( pj ) << "] + 1 = " << static_cast<void*>( pj + 1 ) << std::endl;
+	std::cout << "pk: [" << pk << "] + 1 = " << pk + 1 << std::endl;
+	std::cout << "pl: [" << pl << "] + 1 = " << pl + 1 << std::endl;
+
+	std::cout << __FUNCTION__ << " exit\n";
+}
+
+void subfunc4( )
+{
+	std::cout << __FUNCTION__ << " entry\n";
+
+	int arr[ 4 ]{ 1, 2, 3, 4 };
+
+	for ( int i = 0; i < 4; ++i )
+	{
+		std::cout << *( arr + i ) << ' ';
+	}
+	
+	std::cout << std::endl;
+
+	std::cout << __FUNCTION__ << " exit\n";
 }
