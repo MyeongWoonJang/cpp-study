4607ecbd1dbbf5bbde443781583445274615c20a
orange2458 <asm1133@naver.com>
2022-12-10 13:08:47 +0900

    CS-1 CS-378

--- New Project/New Project/manage_memory.cpp
+++ New Project/New Project/manage_memory.cpp
@@ -3,12 +3,40 @@
 int subfunc1( int );
 char subfunc2( float, char );
 
+template <typename Ty>
+void printmem_stream( const Ty& var, std::ostream& os );  // function template declaration
+template <typename Ty>
+void printmem( const Ty& var );   // function template declaration
+
+// 변수를 전달하면 그 변수의 메모리 개요를 출력합니다.
+template <typename Ty>
+void printmem_stream( const Ty& var, std::ostream& os )    // function template definition
+{
+	// [메모리 시작 주소] ~ [메모리 끝 주소] - 타입 이름 : 타입의 크기 byte, value : 값
+	// ex) [0x7ffc7d318a7c] ~ [0x7ffc7d318a7f] - int : 4 byte, value : 0
+	// 형식으로 메모리를 출력합니다.
+	os << '[' << static_cast<const void*>( &var ) << "] ~ [" << static_cast<const void*>( reinterpret_cast<const char*>( &var ) + sizeof( var ) - 1 )
+		<< "] - " << typeid( var ).name( ) << " : " << sizeof( var ) << " byte, value : " << var << '\n';
+}
+
+template <typename Ty>
+void printmem( const Ty& var )    // function template definition
+{
+	printmem_stream( var, std::cout );
+}
+
 int main( )
 {
 	std::cout << __FUNCTION__ << " entry\n";
 
-	int var1{ subfunc1( 1 ) };
-	char var2{ subfunc2( 1.1f, 'n' ) };
+	int var1{ };
+	char var2{ };
+
+	printmem( var1 );
+	printmem( var2 );
+
+	var1 = subfunc1( 1 );
+	var2 = subfunc2( 1.1f, 'n' );
 
 	std::cout << __FUNCTION__ << " exit\n";
 }
@@ -19,6 +47,10 @@ int subfunc1( int a )
 
 	short b{ 12 };
 	float c{ 3.14f };
+
+	printmem( a );
+	printmem( b );
+	printmem( c );
 
 	std::cout << __FUNCTION__ << " exit\n";
 
@@ -31,6 +63,11 @@ char subfunc2( float d, char e )
 
 	char f{ 'u' };
 	double g{ 2.145134 };
+
+	printmem( d );
+	printmem( e );
+	printmem( f );
+	printmem( g );
 
 	std::cout << __FUNCTION__ << " exit\n";
 
