03370b01fb0ed872f91c6d39198b2243b2fce73f
orange2458 <asm1133@naver.com>
2022-12-11 20:46:04 +0900

    CS-1 CS-191

--- New Project/New Project/dynamic allocation.cpp
+++ New Project/New Project/dynamic allocation.cpp
@@ -23,15 +23,69 @@ void printmem( const Ty& var )    // function template definition
 }
 
 void memory_leak( );
+void dangling_pointer( );
+void* operator new( std::size_t sz );
+void operator delete( void* ptr ) noexcept;
 
 int main( )
 {
-    memory_leak( );
+    int* heap_int = new int{ 123 };
 
-    std::cin.get( );
+    printmem( heap_int );
+    printmem( *heap_int );
+
+    delete heap_int;
+
+    int* heap_arr = new int[5] { 1, 2, 3, 4, 5 };
+
+    printmem( heap_arr );
+    printmem( *heap_arr );
+
+    for ( int i = 0; i < 5; ++i )
+        printmem( heap_arr[ i ] );
+
+    delete[ ] heap_arr;
 }
 
 void memory_leak( )
 {
     char* mem = new char[1'000'000'000] { 0 };
+}
+
+void dangling_pointer( )
+{
+    char* mem = new char[1000] { 0 };
+
+    delete[ ] mem;
+
+    std::cout << mem[ 100 ];
+}
+
+void* operator new( std::size_t sz )
+{
+    std::cout << __FUNCTION__ << " entry\n";
+
+    if ( sz == 0 )
+        ++sz; // avoid std::malloc(0) which may return nullptr on success
+
+    while ( true )
+    {
+        if ( void* ptr = std::malloc( sz ) )
+        {
+            std::cout << __FUNCTION__ << " exit\n";
+            return ptr;
+        }
+
+        if ( std::new_handler nh = std::get_new_handler( ) ) nh( );
+        else throw std::bad_alloc{}; // required by [new.delete.single]/3
+    }
+}
+
+void operator delete( void* ptr ) noexcept
+{
+    std::cout << __FUNCTION__ << " entry\n";
+
+    std::free( ptr );
+
+    std::cout << __FUNCTION__ << " exit\n";
 }
