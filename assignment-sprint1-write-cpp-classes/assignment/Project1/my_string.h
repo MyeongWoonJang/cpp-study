bf4a86d0a23e49dca6e3b9e32ea486d26dc15856
Lcat <ljj7403@naver.com>
2022-12-21 21:00:14 +0900

    CS-1 CS-119 CS-222
    improve parameter names

--- assignment-sprint1-write-cpp-classes/assignment/Project1/my_string.h
+++ assignment-sprint1-write-cpp-classes/assignment/Project1/my_string.h
@@ -6,26 +6,26 @@
 class my_string
 {
 public:
-	const char& at(const size_t n) const;
+	const char& at(const size_t location) const;
 	const char& front() const;
 	const char* c_str() const;
 	const char* data() const;
 	bool empty() const;
 	int size() const;
 	int length() const;
-	void reserve(const size_t n);
+	void reserve(const size_t size);
 	size_t capacity();
 	void shrink_to_fit();
-	void resize(const size_t n);
+	void resize(const size_t size);
 	void clear();
-	void insert(const size_t n, const char* other);
-	void erase(const size_t n, const size_t m);
-	void push_back(const char c);
+	void insert(const size_t location, const char* insert_str);
+	void erase(const size_t location, const size_t number_of_char);
+	void push_back(const char inserted_char);
 	void pop_back();
-	void append(const char c);
+	void append(const char inserted_char);
 	int compare(const char* other) const;
-	void replace(size_t offset, size_t m, size_t k, size_t l, const char* s);
-	const my_string& substr(const size_t n, const size_t m);
+	void replace(size_t src_location, size_t number_of_src, const char* other_str, size_t other_location, size_t number_of_other);
+	const my_string& substr(const size_t location, const size_t number_of_char);
 	int find(const char* str) const;
 
 private:
