def3fc9794fe4b356a631b3f3c3a3ed3ba41af31
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-03-26 18:38:28 +0900

    added freetype(external library) CE-24

--- core/main.cpp
+++ core/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/freetype.hpp>
 
 int main()
 {
+    char ch;
     std::cout << "Hello, World!\n";
+
+    cv::freetype::createFreeType2();
+
+    std::cout << "press any key to exit.\n";
+    std::cin >> ch;
 }
