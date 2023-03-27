ea203fcfc52f92b451f6d9a5f5e8ff9c30f8e7f2
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-03-27 12:52:14 +0900

    Font print test CE-24

--- core/main.cpp
+++ core/main.cpp
@@ -1,13 +1,53 @@
 #include <iostream>
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/freetype.hpp>
+
+void opencv_sample()
+{
+    cv::namedWindow("sample");
+
+    cv::String text = "Funny text inside the box";
+    int fontHeight = 60;
+    int thickness = -1;
+    int linestyle = cv::LINE_8;
+    cv::Mat img(600, 800, CV_8UC3, cv::Scalar::all(0));
+    int baseline=0;
+    cv::Ptr<cv::freetype::FreeType2> ft2;
+    ft2 = cv::freetype::createFreeType2();
+    ft2->loadFontData( "../../resources/GOUDOS.TTF", 0 );
+    cv::Size textSize = ft2->getTextSize(text,
+                                    fontHeight,
+                                    thickness,
+                                    &baseline);
+    if(thickness > 0){
+        baseline += thickness;
+    }
+    // center the text
+    cv::Point textOrg((img.cols - textSize.width) / 2,
+                (img.rows + textSize.height) / 2);
+    // draw the box
+    cv::rectangle(img, textOrg + cv::Point(0, baseline),
+            textOrg + cv::Point(textSize.width, -textSize.height),
+            cv::Scalar(0,255,0),1,8);
+    // ... and the baseline first
+    cv::line(img, textOrg + cv::Point(0, thickness),
+        textOrg + cv::Point(textSize.width, thickness),
+        cv::Scalar(0, 0, 255),1,8);
+    // then put the text itself
+    ft2->putText(img, text, textOrg, fontHeight,
+                cv::Scalar::all(255), thickness, linestyle, true );
+
+    cv::imshow("sample", img);
+    cv::waitKey(25);
+}
 
 int main()
 {
     char ch;
     std::cout << "Hello, World!\n";
 
-    cv::freetype::createFreeType2();
+    opencv_sample();
 
     std::cout << "press any key to exit.\n";
     std::cin >> ch;
